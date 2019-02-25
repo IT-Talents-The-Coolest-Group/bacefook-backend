06bf19ff0b4960b3157e0ec471786354fd6c8236
Ivelina Hristova <ivelinahristova5@gmail.com>
2019-02-25 18:22:18 +0200

    update dumb

--- bacefook_update_dump.sql
+++ bacefook_update_dump.sql
@@ -1,1 +1,6 @@
-id,gender_name
+USE bacefook;
+ALTER TABLE additional_users_info ADD COLUMN  cover_photo_id INT(11) NULL DEFAULT NULL AFTER profile_photo_id;
+ALTER TABLE additional_users_info ADD CONSTRAINT fk_info FOREIGN KEY (id) REFERENCES users(id);
+ALTER TABLE genders ADD UNIQUE (gender_name);
+select * from genders;
+SELECT * FROM additional_users_info;
