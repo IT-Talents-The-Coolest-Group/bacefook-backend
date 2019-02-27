b30f3812b4d582f070c9d72aafa320a43275bec4
Ivelina Hristova <ivelinahristova5@gmail.com>
2019-02-27 19:19:11 +0200

    config a new project

--- Bacefook-backend/src/main/java/com/bacefook/BacefookBackendApplication.java
+++ Bacefook-backend/src/main/java/com/bacefook/BacefookBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BacefookApplication {
+public class BacefookBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BacefookApplication.class, args);
+		SpringApplication.run(BacefookBackendApplication.class, args);
 	}
 
 }
--- Bacefook-backend/src/test/java/com/bacefook/BacefookBackendApplicationTests.java
+++ Bacefook-backend/src/test/java/com/bacefook/BacefookBackendApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BacefookApplicationTests {
+public class BacefookBackendApplicationTests {
 
 	@Test
 	public void contextLoads() {
