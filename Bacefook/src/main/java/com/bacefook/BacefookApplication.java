0b9b03e228fcb666cb1ee5dc8047cd7f6838482f
Veradux <veradux@abv.bg>
2019-02-27 18:31:05 +0200

    adding the project foundation

---
+++ Bacefook/src/main/java/com/bacefook/BacefookApplication.java
@@ -0,0 +1,13 @@
+package com.bacefook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacefookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BacefookApplication.class, args);
+	}
+
+}
---
+++ Bacefook/src/test/java/com/bacefook/BacefookApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bacefook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BacefookApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
