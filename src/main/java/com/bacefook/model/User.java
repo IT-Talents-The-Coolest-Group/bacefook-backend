156f584b8564a374acc1067839c840a82891eb2d
Veradux <veradux@abv.bg>
2019-02-28 21:38:07 +0200

    removed front end files because they are now hosted seperately

--- src/main/java/com/bacefook/model/User.java
+++ src/main/java/com/bacefook/model/User.java
@@ -2,18 +2,25 @@ package com.bacefook.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+@Entity
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name="users")
 public class User {
+	@Id
 	private int id;
 	private String email;
 	private String password;
