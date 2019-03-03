267dc9c27dab5a36e6805454b3058f9318736a60
Ivelina Hristova <ivelinahristova5@gmail.com>
2019-03-03 17:05:05 +0200

    Added class BaseController for ExceptionHandling and package security for crypting password

---
+++ src/main/java/com/bacefook/security/Cryptography.java
@@ -0,0 +1,20 @@
+package com.bacefook.security;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Cryptography {
+	public static String cryptSHA256(String password) throws NoSuchAlgorithmException {
+
+		MessageDigest digest = MessageDigest.getInstance("SHA-256");
+		byte[] byteData = digest.digest(password.getBytes());
+		StringBuilder stringBuilder = new StringBuilder();
+		for (int i = 0; i < byteData.length; i++) {
+			String hex = Integer.toHexString(0xff & byteData[i]);
+			if (hex.length() == 1)
+				stringBuilder.append('0');
+			stringBuilder.append(hex);
+		}
+		return stringBuilder.toString();
+	}
+}
