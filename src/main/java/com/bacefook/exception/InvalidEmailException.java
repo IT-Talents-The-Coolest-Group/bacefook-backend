d862e0d79afef2a1e12bc7129592198ba3c72b51
Ivelina Hristova <ivelinahristova5@gmail.com>
2019-03-04 03:23:10 +0200

    Global Exception Handling

---
+++ src/main/java/com/bacefook/exception/InvalidEmailException.java
@@ -0,0 +1,36 @@
+package com.bacefook.exception;
+
+public class InvalidEmailException extends Exception {
+
+	public InvalidEmailException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidEmailException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidEmailException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidEmailException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidEmailException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7418698483449478966L;
+
+}
--- src/main/java/com/bacefook/utility/UserValidation.java
+++ src/main/java/com/bacefook/utility/UserValidation.java
@@ -5,54 +5,57 @@ import java.time.Period;
 
 import com.bacefook.dto.LoginDTO;
 import com.bacefook.dto.SignUpDTO;
+import com.bacefook.exception.PasswordMatchingException;
+import com.bacefook.exception.InvalidEmailException;
+import com.bacefook.exception.InvalidPasswordException;
 import com.bacefook.exception.InvalidUserCredentialsException;
 
 public class UserValidation {
 
 	private static final int MIN_NAMES_LENGTH = 3;
-	private static final int MIN_AGE = 12;
+	private static final int MIN_AGE = 13;
 	private static final int MAX_AGE = 120;
 	private final static String EMAIL_PATTERN = "^[(a-zA-Z-0-9-\\_\\+\\.)]+@[(a-z-A-z)]+\\.[(a-zA-z)]{2,5}$";
 	private final static String PASSWORD_PATTERN = "^[a-zA-Z0-9]{8,30}$";
-	
-	private void validateEmail(String email) throws InvalidUserCredentialsException {
+
+	private void validateEmail(String email) throws InvalidEmailException {
 		if (isNullOrEmpty(email) || !email.matches(EMAIL_PATTERN)) {
-			throw new InvalidUserCredentialsException("Invalid email format!");
+			throw new InvalidEmailException("Wrong login credentials!");
 		}
 	}
 
-	public void validatePassword(String pass1) throws InvalidUserCredentialsException {
+	public void validatePassword(String pass1) throws InvalidPasswordException {
 		if (isNullOrEmpty(pass1) || !pass1.matches(PASSWORD_PATTERN)) {
-			throw new InvalidUserCredentialsException("Invalid password, must only contain latin letters and numbers");
+			throw new InvalidPasswordException("Wrong login credentials");
 		}
 	}
 
-	private void validateBirthday(LocalDate birthday) throws InvalidUserCredentialsException {
+	private void validateBirthday(LocalDate birthday) throws InvalidUserCredentialsException{
 		Period period = Period.between(birthday, LocalDate.now());
 		if (period.getYears() >= MAX_AGE && period.getYears() < MIN_AGE) {
 			throw new InvalidUserCredentialsException(
-					"Invalid birth date, must be older than " + MIN_AGE + 
-					" and younger than "+ MAX_AGE + "!");
+					"Invalid birth date, must be older than " + (MIN_AGE - 1) + " and younger than " + MAX_AGE + "!");
 		}
 	}
-	
+
 	private void validateName(String name) throws InvalidUserCredentialsException {
 		if (isNullOrEmpty(name) || name.length() < MIN_NAMES_LENGTH) {
 			throw new InvalidUserCredentialsException("Invalid name, must be longer than " + MIN_NAMES_LENGTH + "!");
 		}
 	}
-	
+
 	private boolean isNullOrEmpty(String string) {
 		return string == null || string.isEmpty();
 	}
 
-	public void confirmPassword(String password, String passwordConfirmation) throws InvalidUserCredentialsException {
+	public void confirmPassword(String password, String passwordConfirmation) throws PasswordMatchingException {
 		if (isNullOrEmpty(password) || !password.equals(passwordConfirmation)) {
-			throw new InvalidUserCredentialsException("Credentials do not match!");
+			throw new PasswordMatchingException("Passwords do not match!");
 		}
 	}
-	
-	public void validate(SignUpDTO signUp) throws InvalidUserCredentialsException {
+
+	public void validate(SignUpDTO signUp) throws InvalidUserCredentialsException, PasswordMatchingException,
+			InvalidEmailException, InvalidPasswordException{
 		validateName(signUp.getFirstName());
 		validateName(signUp.getLastName());
 		validateEmail(signUp.getEmail());
@@ -60,18 +63,15 @@ public class UserValidation {
 		confirmPassword(signUp.getPassword(), signUp.getPasswordConfirmation());
 		validateBirthday(signUp.getBirthday());
 	}
-	
-	public void validate(LoginDTO login) throws InvalidUserCredentialsException {
+
+	public void validate(LoginDTO login) throws InvalidPasswordException, InvalidEmailException {
 		if (isNullOrEmpty(login.getEmail())) {
-			throw new InvalidUserCredentialsException("Email must not be empty!");
+			throw new InvalidEmailException("Email must not be empty!");
 		}
-		
+
 		if (isNullOrEmpty(login.getPassword())) {
-			throw new InvalidUserCredentialsException("Password must not be empty!");
+			throw new InvalidPasswordException("Password must not be empty!");
 		}
 	}
 
-	
-
-	
 }
