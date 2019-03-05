f86b2fef3879b9ec333d2a3e8c97576c41a40c0d
Ivelina Hristova <ivelinahristova5@gmail.com>
2019-03-05 17:55:20 +0200

    Update a comment method

--- src/main/java/com/bacefook/service/CommentService.java
+++ src/main/java/com/bacefook/service/CommentService.java
@@ -1,8 +1,11 @@
 package com.bacefook.service;
 
+import java.util.NoSuchElementException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bacefook.exception.ElementNotFoundException;
 import com.bacefook.model.Comment;
 import com.bacefook.repository.CommentsRepository;
 
@@ -14,6 +17,16 @@ public class CommentService {
 	
 	public void saveComment(Comment comment) {
 		commentsRepo.save(comment);
+	}
+	public Comment findCommentById(Integer commentId) throws ElementNotFoundException {
+		//TODO Global Handling
+		//TODO commentId!=null
+		try {
+		Comment comment = commentsRepo.findById(commentId).get();
+		return comment;
+		}catch(NoSuchElementException e) {
+			throw new ElementNotFoundException("No such comment!");
+		}
 	}
 	
 }
