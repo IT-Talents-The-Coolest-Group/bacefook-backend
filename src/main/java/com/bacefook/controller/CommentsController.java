2876c286f4ff58e906b84826500ad69d3505edfc
Ivelina Hristova <ivelinahristova5@gmail.com>
2019-03-05 21:43:37 +0200

    Get all comments by post

--- src/main/java/com/bacefook/controller/CommentsController.java
+++ src/main/java/com/bacefook/controller/CommentsController.java
@@ -13,36 +13,38 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bacefook.dto.CommentContentDTO;
 import com.bacefook.dto.CommentDTO;
-import com.bacefook.dto.PostDTO;
 import com.bacefook.exception.ElementNotFoundException;
 import com.bacefook.exception.UnauthorizedException;
 import com.bacefook.model.Comment;
-import com.bacefook.model.Post;
 import com.bacefook.service.CommentService;
+import com.bacefook.service.PostService;
+import com.bacefook.service.UserService;
 import com.bacefook.utility.TimeConverter;
 
 @RestController
 public class CommentsController {
 	@Autowired
 	CommentService commentsService;
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private PostService postService;
 
 	@PostMapping("/comments")
 	public ResponseEntity<Object> addCommentToPost(@RequestParam("postId") Integer postId,
-			@RequestBody CommentContentDTO commentContentDto, HttpServletRequest request) 
-					throws UnauthorizedException {
+			@RequestBody CommentContentDTO commentContentDto, HttpServletRequest request) throws UnauthorizedException {
 		// throw exception if no user in session
 		// TODO check if comment is a reply on another comment
 		// TODO validate if properties are not empty
-		
+
 		int posterId = SessionManager.getLoggedUser(request).getId();
-		Comment comment = new Comment(posterId, postId, 
-				commentContentDto.getContent(), LocalDateTime.now());
+		Comment comment = new Comment(posterId, postId, commentContentDto.getContent(), LocalDateTime.now());
 
 		// TODO validate with status code
 		commentsService.saveComment(comment);
@@ -66,26 +68,29 @@ public class CommentsController {
 			throw new UnauthorizedException("You are not logged in! Please log in before trying to update your posts.");
 		}
 	}
+
 	@GetMapping("/comments")
-	public ResponseEntity<List<CommentDTO>> getAllCommentsByPost(@RequestHeader("postId") Integer postId,
-			HttpServletRequest request) {
+	public ResponseEntity<List<CommentDTO>> getAllCommentsByPost(@RequestParam("postId") Integer postId,
+			HttpServletRequest request) throws ElementNotFoundException {
 
 		// TODO check if user is friend with poster
 //		SessionManager.getLoggedUser(request);
+		postService.findPostById(postId);
 
-		List<Comment> comments = commentsService.g
-		List<PostDTO> returnedPosts = new ArrayList<>();
+		List<Comment> comments = commentsService.getAllCommentsByPostId(postId);
+		List<CommentDTO> commentsOnPost = new ArrayList<>();
 
-		for (Post post : posts) {
-			String posterFullName = userService.findUserById(post.getPosterId()).getFullName();
+		for (Comment comment : comments) {
+			String posterFullName = userService.findUserById(comment.getPosterId()).getFullName();
 
-			String timeOfPosting = TimeConverter.convertTimeToString(post.getPostingTime());
+			String timeOfPosting = TimeConverter.convertTimeToString(comment.getPostingTime());
 
-			PostDTO postDto = new PostDTO(posterFullName, post.getSharesPostId(), post.getContent(), timeOfPosting);
+			CommentDTO commentDto = new CommentDTO(posterFullName, comment.getCommentedOnId(), comment.getContent(),
+					timeOfPosting);
 
-			returnedPosts.add(postDto);
+			commentsOnPost.add(commentDto);
 		}
-		return new ResponseEntity<>(returnedPosts, HttpStatus.OK);
+		return new ResponseEntity<>(commentsOnPost, HttpStatus.OK);
 
 	}
 
