8a52f5c7a743343ee7b437243229608a9de6353d
Veradux <veradux@abv.bg>
2019-03-10 20:09:02 +0200

    fixed the post content to show picture url if it was uploaded

--- src/main/java/com/bacefook/service/PhotoService.java
+++ src/main/java/com/bacefook/service/PhotoService.java
@@ -53,7 +53,7 @@ public class PhotoService {
 			Map response = cloudinary.uploader().upload(file, ObjectUtils.asMap("public_id", file.getName()));
 			String url = (String) response.get("url");
 
-			int postId = postsService.save(userId, file.getName());
+			int postId = postsService.save(userId, url);
 			Post post = postsService.findById(postId);
 			Photo photo = photosRepo.save(new Photo(post.getId(), url));
 
